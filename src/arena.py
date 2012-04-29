ed94212523369b0fd7d8499c1b549be01213d0cf
Alastair Lynn <arplynn@gmail.com>
2012-04-29 13:46:49 +0100

    Simplify the read-only properties in Arena
    They now use operator.getattr.

--- src/arena.py
+++ src/arena.py
@@ -1,18 +1,15 @@
+from operator import attrgetter
 
 from point import Point
+
 
 class Arena(object):
 	def __init__(self, width, height):
 		self._width = width
 		self._height = height
 
-	@property
-	def width(self):
-		return self._width
-
-	@property
-	def height(self):
-		return self._height
+	width = property(attrgetter('_width'))
+	height = property(attrgetter('_height'))
 
 	def corner_location(self, id):
 		x = self._width * int(id / 2)
